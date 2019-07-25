25d7e352a6fd55000eea6070b5e631f88553c2b5
Matt Trentini <matt.trentini@gmail.com>
2019-07-25 15:49:49 +1000

    QR codes can now be generated from MicroPython across the C boundary!

--- modules/qr/qr.c
+++ modules/qr/qr.c
@@ -3,28 +3,16 @@
 #include "py/runtime.h"
 #include "py/builtin.h"
 
-#include <string.h>
-
 #include "qrcodegen.h"
 
-// This is the function which will be called from Python as example.add_ints(a, b).
-STATIC mp_obj_t qr_add_ints(mp_obj_t a_obj, mp_obj_t b_obj) {
-    // Extract the ints from the micropython input objects
-    int a = mp_obj_get_int(a_obj);
-    int b = mp_obj_get_int(b_obj);
-
-    // Calculate the addition and convert to MicroPython object.
-    return mp_obj_new_int(a + b);
-}
-// Define a Python reference to the function above
-STATIC MP_DEFINE_CONST_FUN_OBJ_2(qr_add_ints_obj, qr_add_ints);
-
+// The maximum row/column is 177 when version 40 QR code are employed
+#define MAX_QR_SIZE (177)
 
 STATIC mp_obj_t encode_text(
         mp_obj_t text_obj) {
     const char* text = mp_obj_str_get_str(text_obj);
 
-    // Text data
+    // Generate QR code
     uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
     uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
     bool ok = qrcodegen_encodeText(text,
@@ -32,31 +20,34 @@ STATIC mp_obj_t encode_text(
         qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
         qrcodegen_Mask_AUTO, true);
     if (!ok) {
-        // Raise exception
+        mp_raise_ValueError("Error generating QR code");
     }
 
-    mp_obj_t row[30];
+    // fixme: Good candidate for static allocation?
+    mp_obj_t column[MAX_QR_SIZE];
+    mp_obj_t row[MAX_QR_SIZE];
 
     int size = qrcodegen_getSize(qr0);
-    //for (int y = 0; y < size; y++) {
+    //assert(size <= MAX_QR_SIZE);
+    for (int y = 0; y < size; y++) {
         for (int x = 0; x < size; x++) {
-            row[x] = mp_obj_new_bool(qrcodegen_getModule(qr0, x, 0));
+            column[x] = mp_obj_new_bool(qrcodegen_getModule(qr0, x, y));
         }
-    //}
+        row[y] = mp_obj_new_tuple(size, column);
+    }
     return mp_obj_new_tuple(size, row);
 }
 MP_DEFINE_CONST_FUN_OBJ_1(encode_text_obj, encode_text);
 
 
-// Define all properties of the example module.
-// Table entries are key/value pairs of the attribute name (a string)
-// and the MicroPython object reference.
-// All identifiers and strings are written as MP_QSTR_xxx and will be
-// optimized to word-sized integers by the build system (interned strings).
+// Define the contents of the module
 STATIC const mp_rom_map_elem_t qr_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_qr) },
-    { MP_ROM_QSTR(MP_QSTR_add_ints), MP_ROM_PTR(&qr_add_ints_obj) },
     { MP_ROM_QSTR(MP_QSTR_encode_text), MP_ROM_PTR(&encode_text_obj) }, // Oli: Maybe prefix module name ahead of encode_text_obj?
+    { MP_ROM_QSTR(MP_QSTR_ECC_LOW), MP_ROM_INT(qrcodegen_Ecc_LOW) },
+    { MP_ROM_QSTR(MP_QSTR_ECC_MEDIUM), MP_ROM_INT(qrcodegen_Ecc_MEDIUM) },
+    { MP_ROM_QSTR(MP_QSTR_ECC_QUARTILE), MP_ROM_INT(qrcodegen_Ecc_QUARTILE) },
+    { MP_ROM_QSTR(MP_QSTR_ECC_HIGH), MP_ROM_INT(qrcodegen_Ecc_HIGH) },
 };
 STATIC MP_DEFINE_CONST_DICT(qr_module_globals, qr_module_globals_table);
 
